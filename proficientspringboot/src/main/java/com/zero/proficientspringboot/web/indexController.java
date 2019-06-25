cbb90d3a06d4c9199fce22efce21600add2c2b3a
张春海 <zhangch1@yonyou.com>
2019-06-25 21:15:17 +0800

    fragment,insert,replace.
    Thymeleaf页面布局
    th:fragment 创建了一段代码片段
    th:insert 和 th:replace 区别，insert 只是加载，replace 是替换。
    Thymeleaf 3.0 推荐使用 th:insert 替换 2.0 的 th:replace。

---
+++ proficientspringboot/src/main/java/com/zero/proficientspringboot/web/indexController.java
@@ -0,0 +1,34 @@
+package com.zero.proficientspringboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：Zero
+ * @date ：Created in 2019/6/25 20:59
+ * @modified By：
+ */
+@Controller
+public class indexController {
+
+    @RequestMapping("/index")
+    public String index () {
+        return "index";
+    }
+
+    @RequestMapping("/fragment")
+    public String fragment(){
+        return "fragment";
+    }
+
+    @RequestMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
