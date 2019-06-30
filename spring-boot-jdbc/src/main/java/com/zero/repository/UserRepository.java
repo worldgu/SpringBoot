22e65d586eb9d1593f05d1202701401fd543ddde
张春海 <15893851179@163.com>
2019-06-30 22:25:32 +0800

    JDBC 单数据源连接, ORM,HikariCP
     JDBC 操作数据库
    JDBC（Java Data Base Connectivity，Java 数据库连接）是一种用于执行 SQL 语句的 Java API，可以为多种关系数据库提供统一访问，它由一组用 Java 语言编写的类和接口组成。JDBC 提供了一种基准，据此可以构建更高级的工具和接口，使数据库开发人员能够编写数据库应用程序。
    Java 程序中使用 JDBC 比较复杂，需要 7 步才能完成数据库的操作：
    
    加载数据库驱动
    建立数据库连接
    创建数据库操作对象
    定义操作的 SQL 语句
    执行数据库操作
    获取并操作结果集
    关闭对象，回收资源

---
+++ spring-boot-jdbc/src/main/java/com/zero/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.zero.repository;
+
+import com.zero.model.User;
+
+import java.util.List;
+
+public interface UserRepository  {
+
+    int save(User user);
+
+    int update(User user);
+
+    int delete(long id);
+
+    List<User> findALL();
+
+    User findById(long id);
+}
---
+++ spring-boot-jdbc/user.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(32) DEFAULT NULL COMMENT '密码',
+  `age`  int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
