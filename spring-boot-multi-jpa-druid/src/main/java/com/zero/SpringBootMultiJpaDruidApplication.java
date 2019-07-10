f4934f31f1d87ca9e5db42338b4bac46331dcc8c
张春海 <15893851179@163.com>
2019-07-10 16:34:51 +0800

    使用原生的druid对Jpa进行多数据源的配置
    Spring Data JPA 中使用 Druid 作为连接池。
    Druid 是一款非常优秀的数据库连接池开源软件，使用 Druid 提供的 druid-spring-boot-starter 可以非常简单地对 Druid 进行集成。Druid 提供了很多预置的功能，非常方便我们对 SQL 进行监控、分析。Druid 对 Spring Boot 2.0 的支持还不够完善，对于使用 Druid 的特殊场景，可以使用 Druid 原生包自行进行封装。

---
+++ spring-boot-multi-jpa-druid/src/main/java/com/zero/SpringBootMultiJpaDruidApplication.java
@@ -0,0 +1,13 @@
+package com.zero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiJpaDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiJpaDruidApplication.class, args);
+    }
+
+}
